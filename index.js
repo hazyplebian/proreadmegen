9e527c2394fd43373f95eea9f7e9547e9cbd68f2
hazyplebian <michael.mangieri@yahoo.com>
2025-03-22 15:51:46 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
 
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "list",
+    name: "license",
+    message: "Select a license",
+    choices: ["BSD", "Apache", "None"],
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information",
+  },
+  {
+    type: "input",
+    name: "screenshot",
+    message: "Provide the screenshot filepath",
+  },
+  {
+    type: "input",
+    name: "video",
+    message: "Provide url to walkthrough video",
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Provide the link to the deployed application",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter test instructions",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successfully created readme!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // Inquirer prompt
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile("README.md", markdown)
+  });
+  // writeToFile();
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license != 'None') {
+    return `![GitHub License](https://img.shields.io/badge/License-${license}-blue)`;
+  } else {
+    return ``;
+  }
+}
+// https://img.shields.io/badge/license-apache-blue
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license != 'None') {
+    return `[License](#License)`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != 'None') {
+    return `# License`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ${renderLicenseLink(data.license)}
+
+  ${renderLicenseSection(data.license)}
 
 `;
 }
